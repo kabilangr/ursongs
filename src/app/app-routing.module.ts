e617d360dea91eec4c02eb6c6910a1c3ef4a06eb
kabilangr <g.r.kabilan@gmail.com>
2020-08-06 00:08:58 +0530

    cool

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SectionComponent } from './section/section.component';
 import { SearchComponent } from './search/search.component';
-import { ContactusComponent } from './contactus/contactus.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: SectionComponent },
   { path: 'search', component: SearchComponent },
-  { path: 'contactus', component: ContactusComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,25 @@ import { SearchComponent } from './search/search.component';
 import { ListComponent } from './list/list.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SectionComponent, SearchComponent, ListComponent, ContactusComponent, SignupComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SectionComponent,
+    SearchComponent,
+    ListComponent,
+    ContactusComponent,
+    SignupComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
